7bfaa1f4da2c4ca3df78e08712a73fc2fff6b128
Elio Bidinost <eo@a.nomal.me>
2026-03-18 10:19:58 -0400

    MCP3008

--- hydromancie_cp_max9814_mcp3008.py
+++ hydromancie_cp_max9814_mcp3008.py
@@ -10,9 +10,9 @@ import digitalio
 # from adafruit_ads1x15 import ADS1115, AnalogIn, ads1x15
 
 # Configuration
-SAMPLE_RATE = 8000 # Samples per second (Hz)
+SAMPLE_RATE = 44100 # Samples per second (Hz)
 RECORD_SECONDS = 10  # Recording duration in seconds
-WAVE_OUTPUT_FILENAME = "#1_mcp_recorded_audio.wav"
+WAVE_OUTPUT_FILENAME = "#4_mcp_recorded_audio.wav"
 NUM_CHANNELS = 1    # Mono recording
 SAMPLE_WIDTH = 2    # 2 bytes for 16-bit audio (numpy 'int16')
 
@@ -65,10 +65,10 @@ while (time.time() - start_time) < RECORD_SECONDS:
     raw_value = chan.value
     # print(chan.value)
     # Convert 10-bit (0-1023) to 16-bit (0-65535)
-    # audio_value = int(raw_value * 64) 
+    audio_value = int(raw_value * 64) 
     # print(audio_value)
     # Append the sample data as bytes
-    frames.append(raw_value) #(audio_value) 
+    frames.append(audio_value) #(audio_value) 
 
 print("Recording stopped.")
 
