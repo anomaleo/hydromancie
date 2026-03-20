b68bb3fc6568b846fe78a412da88ee427475dfb2
EB <eo@a.nomal.me>
2026-03-19 21:53:05 -0400

    FRAME-TESTER

--- hydromancie_perf_timer_scipy_mcp3008.py
+++ hydromancie_perf_timer_scipy_mcp3008.py
@@ -15,9 +15,9 @@ import mcp3008
 # DEFINE WAVFILE PARAMETERS
 NUM_CHANNELS = 1
 SAMPLE_WIDTH_BYTES = 2
-SAMPLE_RATE = 4096
+SAMPLE_RATE = 8196
 DURATION = 5
-FILE_NAME = "#4096_perf__scipy_mcp_audio"
+FILE_NAME = "#FRAMETEST_perf__scipy_mcp_audio"
 WAVE_OUTPUT_FILENAME = FILE_NAME + ".wav"
 MP3_OUTPUT_FILENAME = FILE_NAME + ".mp3"
 
@@ -66,11 +66,22 @@ print("Recording stopped. Frame len(", len(frames))
 
 # Convert the list of samples to a numpy array of int16 type
 # The wave module expects data in a specific format
-audio_data = np.array(np.clip(frames, -32768, 32767),dtype=np.int16)
+# audio_data = np.array(np.clip(frames, -32768, 32767),dtype=np.int16)
 #audio_data = np.array(frames, dtype=np.int16)
 
 # SCIPY WAVE FILE WRITER
-write(WAVE_OUTPUT_FILENAME, SAMPLE_RATE, audio_data)
+# write(WAVE_OUTPUT_FILENAME, SAMPLE_RATE, audio_data)
+
+
+# Write to WAV file
+with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wavfile:
+    wavfile.setnchannels(NUM_CHANNELS)
+    wavfile.setsampwidth(SAMPLE_WIDTH_BYTES)
+    wavfile.setframerate(SAMPLE_RATE)
+    # Pack the list of integers into a bytes object
+    for sample in frames:
+        # '<h' specifies little-endian, signed short (2 bytes)
+        wavfile.writeframes(struct.pack('<h', sample))
 
 # Save the recorded data as a WAV file
 #with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
