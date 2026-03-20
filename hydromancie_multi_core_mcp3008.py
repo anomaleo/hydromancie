ca9eae5c3348f461fb7a392972ae6d134e71f4ee
EB <eo@a.nomal.me>
2026-03-20 10:55:30 -0400

    2W - Pi Zero

--- hydromancie_multi_core_mcp3008.py
+++ hydromancie_multi_core_mcp3008.py
@@ -6,26 +6,28 @@ from scipy.io.wavfile import write
 import struct
 import mcp3008
 import multiprocessing as mp 
-import threading
-
-# DEFINE WAVFILE PARAMETERS
-NUM_CHANNELS = 1
-SAMPLE_WIDTH_BYTES = 2
-SAMPLE_RATE = 11025
-DURATION = 5
-FILE_NAME = "#C2W_11025_FRAMETEST_perf__scipy_mcp_audio"
-WAVE_OUTPUT_FILENAME = FILE_NAME + ".wav"
-MP3_OUTPUT_FILENAME = FILE_NAME + ".mp3"
+# import threading
 
 
 # Initialize SPI bus and the ADC - MCP3008 ADC 
 adc = mcp3008.MCP3008()
 # adc.read([mcp3008.CH0/6/7])
 
-print(f"Recording for {DURATION} seconds at {SAMPLE_RATE} Hz...")
+
+# DEFINE WAVFILE PARAMETERS
+NUM_CHANNELS = 1
+SAMPLE_WIDTH_BYTES = 2
+SAMPLE_RATE = 16000
+DURATION = 5
 FRAMES = int(SAMPLE_RATE * DURATION)
-max_amplitude = (2**15 - 1)
-num_samples = int(SAMPLE_RATE * DURATION)
+MAX_AMPLITUDE = (2**15 - 1)
+
+# FILE OUTPUT 
+FILE_NAME = "#C2W_" + str(SAMPLE_RATE) + "_FRAMETEST_perf__scipy_mcp_audio"
+WAVE_OUTPUT_FILENAME = FILE_NAME + ".wav"
+MP3_OUTPUT_FILENAME = FILE_NAME + ".mp3"
+
+# MCP3008 ADC DATA
 frames = []
 
 
@@ -36,7 +38,7 @@ def do_the_right_thing(seconds, interval=1.0):
         next_tick = start_time + (i +1) * interval
         # do_the_right_thing
         raw_value = adc.read([mcp3008.CH7])
-        frames.append((raw_value[0] / 1023) * max_amplitude)
+        frames.append((raw_value[0] / 1023) * MAX_AMPLITUDE)
         # Sleep until the next tick
         sleep_time = next_tick - time.perf_counter()
         if sleep_time > 0:
@@ -47,6 +49,7 @@ if __name__ == "__main__":
 
 
 
+    print(f"Recording for {DURATION} seconds at {SAMPLE_RATE} Hz...")
 
     print("TIME INTERVAL: ", 1 / SAMPLE_RATE)
     do_the_right_thing(DURATION, (1 / SAMPLE_RATE))
@@ -61,15 +64,14 @@ if __name__ == "__main__":
     # SCIPY WAVE FILE WRITER
     write(WAVE_OUTPUT_FILENAME, SAMPLE_RATE, audio_data)
 
-
-# Save the recorded data as a WAV file
-#with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
-#    wf.setnchannels(NUM_CHANNELS)
-#    wf.setsampwidth(SAMPLE_WIDTH_BYTES)
-#    wf.setframerate(SAMPLE_RATE)
-#    wf.setnframes(FRAMES)
-#    wf.writeframes(b''.join(audio_data)) # (frames.tobytes())
-#    wf.close()
+    # Save the recorded data as a WAV file
+    #with wave.open(WAVE_OUTPUT_FILENAME, 'wb') as wf:
+    #    wf.setnchannels(NUM_CHANNELS)
+    #    wf.setsampwidth(SAMPLE_WIDTH_BYTES)
+    #    wf.setframerate(SAMPLE_RATE)
+    #    wf.setnframes(FRAMES)
+    #    wf.writeframes(b''.join(audio_data)) # (frames.tobytes())
+    #    wf.close()
 
     print(f"File '{WAVE_OUTPUT_FILENAME}' created successfully.")
 
