1ace2a186cc74a22d82158d8c81f1afe36405f8c
Sensor Lab <sensor.lab+hydromancie@concordia.ca>
2026-04-04 21:59:47 -0400

    Pi Camera logic and script

--- hydromancie_zero.py
+++ hydromancie_zero.py
@@ -4,23 +4,37 @@ import board
 import digitalio
 # ACCESS FILE SYSTEM
 import os
-print(os.getcwd())
+# print(os.getcwd())
 
 # PREFIX_FILE
 PREFIX_FILE = "hydromancie_prefix.txt"
 
 # STATUS RGB LED
-r = digitalio.DigitalInOut(board.D18)
+r = digitalio.DigitalInOut(board.D17)
 r.direction = digitalio.Direction.OUTPUT
-g = digitalio.DigitalInOut(board.D18)
+g = digitalio.DigitalInOut(board.D27)
 g.direction = digitalio.Direction.OUTPUT
-b = digitalio.DigitalInOut(board.D18)
+b = digitalio.DigitalInOut(board.D22)
 b.direction = digitalio.Direction.OUTPUT
+# RASPI GND PIN 9 | 14
+# STATUS LEDS OFF
+r.value = g.value = b.value = 0
 
 # SHARED ACTIVATE SIGNAL - ARDUINO RPI
-activate = digitalio.DigitalInOut(board.D4)
-activate.direction = digitalio.Direction.INPUT
-activate.pull = digitalio.Pull.UP
+we_are_go = digitalio.DigitalInOut(board.D4)
+we_are_go.direction = digitalio.Direction.INPUT
+we_are_go.pull = digitalio.Pull.UP
+
+# ACTIVATE CONTROL VARIABLE
+activate = 1 # we are a go ...
+_prefix = -1 # RECORD FILE PREFIX NUMBER 
+
+
+def debug_status(led, flash, t):
+    for _ in range(flash):
+        led.value = not led.value
+        time.sleep(t)
+        print("bleeped")
 
 
 def get_file_prefix():
@@ -49,15 +63,49 @@ def set_file_prefix(pre):
 
 
 if __name__ == '__main__':
-    # FIRST: RETRIEVE LATEST PREFIX
-    _prefix = get_file_prefix()
-    if _prefix is not None:
-        print(_prefix)
-    time.sleep(5)
-    print(set_file_prefix(_prefix))
 
-    #if set_file_prefix(_prefix):
-     #   print("File Prefix Update")
+    # ATTENDING GLOBAL ACTIVATION SIGNAL - EXTERNAL BUTTON SIGNAL
+    if not we_are_go.value:
+        time.sleep(0.375)
+        if not activate:
+            activate = 1
+        else:
+            print(f"We are go to go: access prefix_file, update filename and initiate record session")
+            debug_status(g, 4, 0.127) # GREEN LED ON = RECORDING OFF
+            activate = 0
 
-    while True:
-        led.value = not button.value # light when button is pressed!
+
+    if activate:
+
+        # FIRST: RETRIEVE LATEST PREFIX
+        _prefix = get_file_prefix()
+        if _prefix is None:
+            print(f"ERROR: CHECK PREFIX_FILE!")
+            while True:
+                debug_status(b, 7, 0.067)
+        else:
+            print(f"We are go to go: access prefix_file, update filename and initiate record session")
+
+        # START, SETUP CAMERA RECORDING SESSION...
+
+        time.sleep(5)
+        debug_status(r, 4, 0.127) # RED LED ON = RECORDING ON
+
+        # CLEAN-UP CAMERA RECORDING SESSION...
+
+
+        # NEXT: SAVE RECORDING, UPDATE PREFIX_FILE & REINIT RECORDING (activate = 1)
+        _status = set_file_prefix(_prefix)
+        if _status is None:
+            print(f"ERROR: CHECK PREFIX_FILE!")
+            while True:
+                debug_status(b, 7, 0.067)
+        else:
+            print(f"We are go to go: access prefix_file, update filename and initiate record session")
+
+        # 
+        #if set_file_prefix(_prefix):
+        #   print("File Prefix Update")
+
+        #while True:
+        #    led.value = not button.value # light when button is pressed!
