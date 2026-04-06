06b5467229084511bf3c783f49085641d2aaf171
Sensor Lab <sensor.lab+hydromancie@concordia.ca>
2026-04-05 21:06:27 -0400

    Need POWER for Camera

--- test_picamera_1.py
+++ test_picamera_1.py
@@ -3,10 +3,10 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import PyavOutput
 picam2 = Picamera2()
-config = picam2.create_video_configuration({'size': (640, 480), 'format': 'YUV420'})
+config = picam2.create_video_configuration({'size': (320, 240), 'format': 'YUV420'})
 picam2.configure(config)
 time.sleep(2)
-encoder = H264Encoder(bitrate=10000000)
+encoder = H264Encoder(bitrate=1000000) # 1 MBP = 1000000 | 10 MBPS = 10 000 000
 picam2.start()
 time.sleep(2)
 # encoder.audio = True
