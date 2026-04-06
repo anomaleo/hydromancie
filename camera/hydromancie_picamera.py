e32fd3b0f5e325d9813188e72034b98962eef0d4
Elio Bidinost <62852884+anomaleo@users.noreply.github.com>
2026-04-06 12:03:51 -0400

    Update hydromancie_picamera.py

--- camera/hydromancie_picamera.py
+++ camera/hydromancie_picamera.py
@@ -2,20 +2,24 @@ import time
 import threading
 from picamera2 import Picamera2
 from libcamera import Transform
-from picamera2.encoders import JpegEncoder
+from picamera2.encoders import H264Encoder
+# from picamera2.encoders import JpegEncoder
+# from picamera2.outputs import PyavOutput
+
 
 picam2 = Picamera2()
 video_config = picam2.create_video_configuration({"format": "YUV420", "size": (640, 480)}, transform=Transform(hflip=True, vflip=True))
 picam2.configure(video_config)
-encoder = JpegEncoder(q=73)
+# encoder = JpegEncoder(q=73)
+encoder = H264Encoder(bitrate=10000000) # 1 MBP = 1000000 | 10 MBPS = 10 000 000
 picam2.start()
 
 # Flag to stop the thread
 recording = True
 
 def record_video():
-    """Function to be run in a separate thread"""
-    picam2.start_recording(encoder, 'test-again2.mjpeg')
+    # picam2.start_recording(encoder, 'test-again3.mjpeg')
+    picam2.start_recording(encoder, 'test-again3.h264')
     
     print("Recording started...")
     time.sleep(10)
