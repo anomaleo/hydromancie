a4059d0fb53391e58f96471a6b6ba8525d428306
Sensor Lab <sensor.lab+hydromancie@concordia.ca>
2026-04-05 20:37:56 -0400

    Camera

--- test_picamera_1.py
+++ test_picamera_1.py
@@ -3,14 +3,14 @@ from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 from picamera2.outputs import PyavOutput
 picam2 = Picamera2()
-config = picam2.create_video_configuration({'size': (1296, 972), 'format': 'YUV420'})
+config = picam2.create_video_configuration({'size': (640, 480), 'format': 'YUV420'})
 picam2.configure(config)
 time.sleep(2)
 encoder = H264Encoder(bitrate=10000000)
 picam2.start()
 time.sleep(2)
 # encoder.audio = True
-output = PyavOutput("test.mp4")
+output = PyavOutput("anomaleo1.mp4")
 picam2.start_recording(encoder, output)
 time.sleep(5)
 picam2.stop_recording()
