ea1c022824d5db3c11a6049193cc4d9b981c1784
Elio Bidinost <62852884+anomaleo@users.noreply.github.com>
2026-04-06 12:17:47 -0400

    Update hydromancie_zero.py

--- hydromancie_zero.py
+++ hydromancie_zero.py
@@ -79,48 +79,49 @@ def set_file_prefix(pre):
 
 if __name__ == '__main__':
 
-    # ATTENDING GLOBAL ACTIVATION SIGNAL - EXTERNAL BUTTON SIGNAL
-    if not we_are_go.value:
-        time.sleep(0.375)
-        if not activate:
-            activate = 1
-        else:
-            print(f"We are go to go: access prefix_file, update filename and initiate record session")
-            debug_status(g, 4, 0.127) # GREEN LED ON = RECORDING OFF
-            activate = 0
+    while True:
+        # ATTENDING GLOBAL ACTIVATION SIGNAL - EXTERNAL BUTTON SIGNAL
+        if not we_are_go.value:
+            time.sleep(0.375)
+            if not activate:
+                activate = 1
+            else:
+                print(f"We are go to go: access prefix_file, update filename and initiate record session")
+                debug_status(g, 4, 0.127) # GREEN LED ON = RECORDING OFF
+                activate = 0
 
 
-    if activate:
+        if activate:
 
-        # FIRST: RETRIEVE LATEST PREFIX
-        _prefix = get_file_prefix()
-        if _prefix is None:
-            print(f"ERROR: CHECK PREFIX_FILE!")
-            while True:
-                debug_status(b, 7, 0.067)
-        else:
-            print(f"We are go to go: access prefix_file, update filename and initiate record session")
+            # FIRST: RETRIEVE LATEST PREFIX
+            _prefix = get_file_prefix()
+            if _prefix is None:
+                print(f"ERROR: CHECK PREFIX_FILE!")
+                while True:
+                    debug_status(b, 7, 0.067)
+            else:
+                print(f"We are go to go: access prefix_file, update filename and initiate record session")
 
-        # START, SETUP CAMERA RECORDING SESSION...
-        _namer = f"hydromancie_{_prefix}.h264"
-        picam2.start_recording(encoder, _namer)
-        print("Recording started...")
-        time.sleep(10)
-        picam2.stop_recording()
-        print("Recording stopped.")
-        debug_status(r, 4, 0.127) # RED LED ON = RECORDING ON
+            # START, SETUP CAMERA RECORDING SESSION...
+            _namer = f"hydromancie_{_prefix}.h264"
+            picam2.start_recording(encoder, _namer)
+            print("Recording started...")
+            time.sleep(10)
+            picam2.stop_recording()
+            print("Recording stopped.")
+            debug_status(r, 4, 0.127) # RED LED ON = RECORDING ON
 
-        # CLEAN-UP CAMERA RECORDING SESSION...
+            # CLEAN-UP CAMERA RECORDING SESSION...
 
 
-        # NEXT: SAVE RECORDING, UPDATE PREFIX_FILE & REINIT RECORDING (activate = 1)
-        _status = set_file_prefix(_prefix)
-        if _status is None:
-            print(f"ERROR: CHECK PREFIX_FILE!")
-            while True:
-                debug_status(b, 7, 0.067)
-        else:
-            print(f"We are go to go: access prefix_file, update filename and initiate record session")
+            # NEXT: SAVE RECORDING, UPDATE PREFIX_FILE & REINIT RECORDING (activate = 1)
+            _status = set_file_prefix(_prefix)
+            if _status is None:
+                print(f"ERROR: CHECK PREFIX_FILE!")
+                while True:
+                    debug_status(b, 7, 0.067)
+            else:
+                print(f"We are go to go: access prefix_file, update filename and initiate record session")
 
         # 
         #if set_file_prefix(_prefix):
